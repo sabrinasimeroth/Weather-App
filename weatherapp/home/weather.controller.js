427d717881f04f775c095d10139b7797ef9a4a22
Sabrina  Simeroth <SabrinaSimeroth@Macintosh-3.local>
2017-03-16 17:14:02 -0700

    added functionality and styling

--- weatherapp/home/weather.controller.js
+++ weatherapp/home/weather.controller.js
@@ -13,12 +13,17 @@
       vm.apiKey = '519889fb71f27909ea9936375edcae8f';
       vm.search = '';
 
-      vm.fetch = function(){
-        $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + vm.search + '&APPID=' + vm.apiKey)
+      // use API key to pull in data from Open Weather Map
+      vm.fetch = function(search){
+        $http.get('http://api.openweathermap.org/data/2.5/weather?q=' + search + '&units=imperial&appid=' + vm.apiKey)
         .then(function(response){
           vm.weather = response.data;
+
+          vm.history.push({
+            name: response.data.name,
+            timestamp: new Date()
+          })
         });
       };
-
     }
   })();
